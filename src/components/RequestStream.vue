f511347c601b29f639aff82c4b66d0a02f2d00f0
ma06rii <ma06rii@live.co.uk>
2020-02-08 14:54:05 +0000

    Add seperate pages for 'request' and 'supply' streams

---
+++ src/components/RequestStream.vue
@@ -0,0 +1,27 @@
+<template id="requestStream">
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">Request Stream</div>
+    </v-ons-toolbar>
+    <p style="text-align: center">
+      This is the first page
+      <v-ons-button @click="push">Push Page 2</v-ons-button>
+    </p>
+  </v-ons-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import SupplyStream from '@/components/SupplyStream.vue'
+
+export default {
+    name:'requestStream',
+    methods: {
+    push() {
+      this.$emit('push-page', SupplyStream);
+    }
+  }
+}
+</script>
