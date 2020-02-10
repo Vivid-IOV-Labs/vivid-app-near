cc7ead100aea080a185eecbae6ef83506d8a6def
ma06rii <ma06rii@live.co.uk>
2020-02-10 00:24:17 +0000

    Increase the zoom for when the geolocation cannot be found.

--- src/components/RequestStream.vue
+++ src/components/RequestStream.vue
@@ -87,7 +87,7 @@ export default {
         onLocationError(e) {
            this.$ons.notification.alert(e.message)
 
-           this.map.setView([51.520748, -0.085040], 6)
+           this.map.setView([51.520748, -0.085040], 24)
 
 
         },
