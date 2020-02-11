f8cc4a1ef338083c9865cf7b93fe22cc149833cd
ma06rii <ma06rii@live.co.uk>
2020-02-11 21:24:07 +0000

    Add geosearch code to the leaflet js map

--- src/components/RequestStream.vue
+++ src/components/RequestStream.vue
@@ -3,16 +3,40 @@
     <v-ons-toolbar>
         <div class="center">Request Stream</div>
     </v-ons-toolbar>
+    <v-ons-list>
+        <v-ons-list-item>
+            Adjust the range size:
+            <v-ons-row>
+                <!-- <v-ons-col width="40px" style="text-align: center; line-height: 31px;">
+            <v-ons-icon icon="md-volume-down"></v-ons-icon>
+          </v-ons-col> -->
+                <v-ons-col>
+
+                    <v-ons-range style="width: 100%;"></v-ons-range>
+
+                </v-ons-col>
+                <!-- <v-ons-col width="40px" style="text-align: center; line-height: 31px;">
+            <v-ons-icon icon="md-volume-up"></v-ons-icon>
+          </v-ons-col> -->
+            </v-ons-row>
+            <!-- Volume: {{ volume }} <span v-show="Number(volume) > 80">&nbsp;(careful, that's loud)</span> -->
+        </v-ons-list-item>
+    </v-ons-list>
 
     <div id="map" class="map"></div>
+
     <section id="nav_buttons" style="text-align: center;">
 
+        <!-- <div style="display:block">
+            <v-ons-button style="margin-top: 1em; width: 8em" @click="pushToViewStreamPage()">Send Request</v-ons-button>
+        </div> -->
+
         <div style="display:block">
-            <v-ons-button style="margin-top: 1em; width: 8em" @click="pushToViewStreamPage()">View Stream</v-ons-button>
+            <v-ons-button style="margin-top: 1em; width: 8em" @click="pushToViewStreamPage()">Send Request</v-ons-button>
         </div>
-        <div style="display:block">
+        <!-- <div style="display:block">
             <v-ons-button style="margin-top: 1em; width: 8em" @click="pushToSupplyStreamPage()">Supply Stream</v-ons-button>
-        </div>
+        </div> -->
     </section>
 </v-ons-page>
 </template>
@@ -21,6 +45,7 @@
 @import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 @import '../../node_modules/leaflet/dist/leaflet.css';
 @import '../css/player.css';
+@import '../../node_modules/leaflet-geosearch/assets/css/leaflet.css';
 
 video {
     width: 100%;
@@ -36,7 +61,7 @@ body {
 html,
 body,
 #map {
-    height: 60vh;
+    height: 50vh;
     width: 100vw;
 }
 
@@ -54,6 +79,11 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import * as L from 'leaflet';
 import 'leaflet-defaulticon-compatibility';
 
+import {
+    GeoSearchControl,
+    EsriProvider
+} from 'leaflet-geosearch';
+const myProvider = new EsriProvider();
 
 export default {
     name: 'requestStream',
@@ -66,7 +96,12 @@ export default {
                 enableHighAccuracy: true,
                 timeout: 5000,
                 maximumAge: 0
-            }
+            },
+            myLocation:null,
+            myLocationCircle: null,
+            geoSearchLocation: null,
+            geoSearchLocationCircle: null,
+            defaultRadius:92
         }
     },
     methods: {
@@ -76,22 +111,39 @@ export default {
         pushToSupplyStreamPage() {
             this.$emit('push-page', SupplyStream);
         },
+        geoSearchEvent(_data) {
+            console.log(_data)
+            console.log(arguments)
+
+            if (this.myLocation) this.map.removeLayer(this.myLocation);
+            if (this.myLocationCircle) this.map.removeLayer(this.myLocationCircle);
+
+            if (this.geoSearchLocation) this.map.removeLayer(this.geoSearchLocation);
+            if (this.geoSearchLocationCircle) this.map.removeLayer(this.geoSearchLocationCircle);
+
+            this.geoSearchLocation = L.marker([_data.location.y, _data.location.x]).addTo(this.map)
+            this.geoSearchLocationCircle = L.circle([_data.location.y, _data.location.x], this.defaultRadius).addTo(this.map)
+            this.map.setView([_data.location.y, _data.location.x], 15);
+
+        },
         onLocationFound(e) {
             var radius = e.accuracy;
 
-            L.marker(e.latlng).addTo(this.map)
-                //.bindPopup("You are within " + radius + " meters from this point").openPopup();
+            this.myLocation = L.marker(e.latlng).addTo(this.map)
+            //.bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-            L.circle(e.latlng, radius).addTo(this.map);
+            this.myLocationCircle = L.circle(e.latlng, radius).addTo(this.map);
         },
         onLocationError(e) {
-           this.$ons.notification.alert(e.message)
+            this.$ons.notification.alert(e.message)
 
-           this.map.setView([51.520748, -0.085040], 15)
-
+            this.map.setView([51.520748, -0.085040], 15)
 
         },
         initMap() {
+
+            console.log(L)
+
             // this.map = L.map('map').setView([38.63, -90.23], 12);
             // this.tileLayer = L.tileLayer(
             //     'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png', {
@@ -101,18 +153,41 @@ export default {
             // );
             // this.tileLayer.addTo(this.map);
 
-            this.map = L.map('map').fitWorld();
+            this.map = L.map('map').setView([51.520748, -0.085040], 15);
 
             this.map.locate({
                 setView: true,
-                maxZoom: 24
+                maxZoom: 20
             });
+
+            new GeoSearchControl({
+                provider: myProvider, // required
+                showMarker: true, // optional: true|false  - default true
+                showPopup: false, // optional: true|false  - default false
+                marker: { // optional: L.Marker    - default L.Icon.Default
+                    icon: new L.Icon.Default(),
+                    draggable: false,
+                },
+                // popupFormat: ({
+                //     query,
+                //     result
+                // }) => result.label, // optional: function    - default returns result label
+                maxMarkers: 1, // optional: number      - default 1
+                retainZoomLevel: false, // optional: true|false  - default false
+                animateZoom: true, // optional: true|false  - default true
+                autoClose: true, // optional: true|false  - default false
+                searchLabel: 'Enter address', // optional: string      - default 'Enter address'
+                keepResult: false
+            }).addTo(this.map);
+
+            this.map.on('geosearch/showlocation', this.geoSearchEvent)
 
         },
         initLayers() {
 
             this.tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWEwNnJpaSIsImEiOiJjazZkeTU2NnAxbWF4M2xxajN6NWIyb2l6In0.4iTjEpS8cIa_Um3zhE9keQ', {
                 //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                attribution: false,
                 maxZoom: 36,
                 id: 'mapbox/streets-v11'
             }).addTo(this.map);
