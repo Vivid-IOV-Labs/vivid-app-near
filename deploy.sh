2038544d102d921184db86953c7e40d8d105e499
ma06rii <ma06rii@live.co.uk>
2020-02-14 16:32:45 +0000

    fix deploy code

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,11 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Vivid-IOV-Labs/Vivid-App.github.io.git master
+
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:Vivid-IOV-Labs/Vivid-App.git master:gh-pages
+
 
 cd -
